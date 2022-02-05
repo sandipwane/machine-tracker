20f142e8d3839ce6943af4ad5a39238be832e3e7
Sandip Wane <sandip.wane@6sense.com>
2022-02-05 21:24:10 +0530

    Define Custome Mocks

--- server.js
+++ server.js
@@ -3,9 +3,24 @@ const { ApolloServer } = require('apollo-server-express');
 const typeDefs =  require('./typeDefs');
 const resolvers = require('./resolvers');
 
+const mocks = {
+  Machine: () => {
+    return { name: () => "Machine Name" };
+  },
+  Sensor: () => {
+    return { name: () => "Sensor Name" };
+  },
+  Position: () => {
+    return { latitude: () => 45.516, longitude: () => -122.636 };
+  },
+  DateTime: () => {
+    return new Date();
+  }
+};
+
 async function startServer() {
   const app = express();
-  const apolloServer = new ApolloServer({ typeDefs, resolvers });
+  const apolloServer = new ApolloServer({ typeDefs, resolvers, mocks });
   await apolloServer.start();
   apolloServer.applyMiddleware({ app, path: '/api' });
   app.listen({ port: 4000 }, () => {
