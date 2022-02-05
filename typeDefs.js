4710ded538cf3ac0951d450a4383ae47d4d93934
Sandip Wane <sandip.wane@6sense.com>
2022-02-05 21:23:12 +0530

    Type Definitions - Machine - Sensor

--- typeDefs.js
+++ typeDefs.js
@@ -1,14 +1,46 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+  scalar DateTime
+
   type Book {
     title: String
     author: String
   }
 
+  type Machine {
+    id: ID!
+    name: String
+    sensors: [Sensor]
+    lastKnownPosition: GPSPosition
+  }
+
+  type Sensor {
+    id: ID!
+    name: String
+    machine: Machine!
+  }
+
+  type Position {
+    latitude: Float
+    longitude: Float
+  }
+
+  type GPSPosition {
+    id: ID!
+    location: Position
+  }
+
+  type SensorDataPoint {
+    timestamp: DateTime!
+    value: Float!
+  }
+
   type Query {
     hello: String
     books: [Book]
+    machines: [Machine!]
+    sensorData:[SensorDataPoint]
   }
 `;
 
