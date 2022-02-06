373784bc569c517a5c8dcec194fa59700b06a71e
Sandip Wane <sandip.wane@6sense.com>
2022-02-06 13:39:16 +0530

    Setup Basic Vue App with Apollo

---
+++ src/machine-tracker-ui/src/components/HelloWorld.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+
+  <div  v-for="machine in machines" :key="machine.id">
+    <h2>{{ machine.name }}</h2>
+    <p>{{ machine.id }}</p>
+  </div>
+
+  </div>
+</template>
+
+<script>
+
+import gql from 'graphql-tag'
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  apollo:{
+    machines: gql`
+      query {
+        machines {
+          id
+          name
+        }
+      }
+    `
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
