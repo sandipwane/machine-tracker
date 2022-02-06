2dfb5389d5321631e8d360daff4e401b236ec4c5
Sandip Wane <sandip.wane@6sense.com>
2022-02-06 21:44:32 +0530

    Rename File

--- src/machine-tracker-ui/src/views/Home.vue
+++ src/machine-tracker-ui/src/views/Home.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Machine List"/>
+    <Home />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import Home from '@/components/Home.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    Home
   }
 }
 </script>
